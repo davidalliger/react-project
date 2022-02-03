a0f5595c76ab11b17b07114ea3c9a6160624c44a
David Alliger <alligerdavidj@gmail.com>
2022-02-03 15:49:42 -0600

    Added login route.

---
+++ backend/db/seeders/20220203184555-demo-user.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        username: 'Demo',
+        email: 'demo@demo.demo',
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo'] }
+    }, {});
+  }
+};
