0a8c9aade9012f7da617af717bc7bf01fe3aead8
David Alliger <alligerdavidj@gmail.com>
2022-02-07 21:40:15 -0600

    Finished splash page.

---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,27 @@
+import './SplashPage.css'
+
+const SplashPage = () => {
+    return (
+        <>
+            <div id='splash-image-container'>
+                <div id='splash-image-overlay'></div>
+                <div id='splash-image-content'>
+                    <h1 id='splash-image-text'>
+                        Make your next trip spooktacular!
+                    </h1>
+                    <button id='splash-image-button'>
+                        Find a new haunt
+                    </button>
+                </div>
+                <img src='/images/hauntedhouse.jpg' id='splash-image' />
+            </div>
+            <div id='credit-text-div'>
+                <p className='credit-text'>
+                    Image by <a target='_blank' className='credit-text-link' href="https://pixabay.com/users/tama66-1032521/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3237114">Peter H</a> from <a target='_blank' className='credit-text-link' href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3237114">Pixabay</a>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default SplashPage;
